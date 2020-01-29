24f082fdde29657e26c9d9f501becbe284bd3c09
camila <camila.valdi.r@gmail.com>
2020-01-29 13:38:12 -0300

    modificación en listar_automovil

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('galeria/', views.galeria, name="galeria"),
     path('formulario/', views.formulario, name="formulario"),
     path('vehiculos/', views.vehiculos, name="vehiculos"),
-    path('listar_automoviles/', views.listar_automoviles, name='listar_autmoviles'),
+    path('listar_automoviles/', views.listar_automoviles, name="listar_autmoviles"),
     path('eliminar-auto/<id>/', views.eliminar_automovil, name="eliminar_automovil"),
-   
-]
+    path('modificar_automovil/<id>/', views.modificar_automovil, name="modificar_automovil" ),
+    
+    ]
